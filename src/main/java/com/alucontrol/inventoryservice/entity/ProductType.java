2fb5e590065cf3a89e22bffc1d72a8f65ee3a975
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-08 21:50:11 +0000

    Create ProductType.java
    Act:
    
    -> The ENUM  improves code readability, security and maintenance.
    By using it, you avoid the use of "magic" constants (like numbers or arbitrary strings), making the code more robust and easy to understand.

---
+++ src/main/java/com/alucontrol/inventoryservice/entity/ProductType.java
@@ -0,0 +1,8 @@
+package com.alucontrol.inventoryservice.entity;
+
+// Um enum (abreviação de "enumeration") é um tipo de dados especial em Java
+// que permite representar um conjunto de valores constantes predefinidos.
+public enum ProductType {
+    SALE,   // Produto para venda
+    RENT    // Produto para aluguel
+}
