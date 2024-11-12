9b52c9454e42867905d19af9cfc4da70ccf7f1e7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-12 21:07:55 +0000

    Update ReadProductServices.java
    Act:
    -> method created to check the stock

--- src/main/java/com/alucontrol/inventoryservice/services/business/ReadProductServices.java
+++ src/main/java/com/alucontrol/inventoryservice/services/business/ReadProductServices.java
@@ -1,6 +1,7 @@
 package com.alucontrol.inventoryservice.services.business;
 
 import com.alucontrol.inventoryservice.entity.Product;
+import com.alucontrol.inventoryservice.exceptions.ResourceNotFoundException;
 import com.alucontrol.inventoryservice.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,17 @@ public class ReadProductServices {
 
     public Product findById(Long id) {
         return productRepository.findById(id).orElseThrow();
+    }
+
+    public boolean hasSufficientStock(Long productId, Integer quantity) {
+
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + productId + " not found"));
+
+        System.out.println("Acessando o Produto ID: " + productId);
+        System.out.println("Quantidade Solicitada " + quantity);
+        System.out.println("Quantidade Disponivel: " + product.getQtyAvailable());
+
+        return product.getQtyAvailable() >= quantity;
     }
 }
