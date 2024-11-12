aa7a3ea8f41be575e67d46b867fccaab2d3e36dd
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-11-12 09:33:13 +0000

    Update Product.java
    Act:
    -> to reduce the complexity, I will change the approach to use IDENTITY

--- src/main/java/com/alucontrol/inventoryservice/entity/Product.java
+++ src/main/java/com/alucontrol/inventoryservice/entity/Product.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private ProductType type; //From Enum
 
